28060972ff63ab34ca42d7d25a3481096745fd58
Gopinath Ayyavu <gopinath0028@gmail.com>
2026-04-06 21:21:14 +0530

    title bar

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
 // For this project, each page handles its own logic for better readability.
 // This file is intentionally minimal but kept for structure.
 console.log("FinTrack App Loaded");
+
+
+function minimize() {
+    Neutralino.window.minimize();
+}
+
+function maximize() {
+    Neutralino.window.maximize();
+}
+
+function closeApp() {
+    Neutralino.app.exit();
+}
